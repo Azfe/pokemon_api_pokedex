42c03480d05ff6af8e8ef14ea37ebcb8f7075fc7
Alex Zapata <alexzapata1984@gmail.com>
2025-06-20 14:29:14 +0200

    Configure environment + configure TailwindCSS

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,24 +1,19 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
-/*import { ShoppingCartProvider } from '../../Context'*/
-import Home from '../Home'
-/*
+import Home from '../Home/index'
 import Navbar from '../../Components/Navbar'
-import CheckoutSideMenu from '../../Components/CheckoutSideMenu'*/
+import PokemonCard from '../../Components/PokemonCard'
+/*
+import SearchBar from '../../Components/SearchBar'
+*/
 import PokemonSearchApp from '../../Components/PokemonSearchApp'
+import { PokedexProvider } from '../../Context'
 import './App.css'
 
-const AppRoutes = () => {
 
+const AppRoutes = () => {
   const routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/clothes', element: <Home /> },
-    /*{ path: '/electronics', element: <Home /> },
-    { path: '/furnitures', element: <Home /> },*/
+    { path: '/', element: <Home /> },    
     { path: '/pokemon-card', element: <PokemonCard /> },
-    { path: '/my-order', element: <MyOrder /> },
-    /*{ path: '/my-orders', element: <MyOrders /> },
-    { path: '/my-orders/last', element: <MyOrder /> },
-    { path: '/my-orders/:id', element: <MyOrder /> },*/
     /*{ path: '/signin', element: <SignIn /> },*/
     /*{ path: '/*', element: <NotFound /> }*/
   ])
@@ -29,41 +24,16 @@ const AppRoutes = () => {
 const App = () => {
 
   return (
-    <ShoppingCartProvider>
+    <PokedexProvider>
       <BrowserRouter>
-        <AppRoutes />
         <Navbar />
-        <CheckoutSideMenu />
+        <AppRoutes />
+        {/*
+        <SearchBar />
+        */}        
       </BrowserRouter>
-    </ShoppingCartProvider>
+    </PokedexProvider>
   )
 }
 
 export default App
-
-/*
-import { useState } from 'react'
-import reactLogo from './../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>   
-    </>
-  )
-}
-
-export default App
-*/
