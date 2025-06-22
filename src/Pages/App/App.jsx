318f0b9e70914ae8cbf69c83f422b21ca6ed6b97
Alex Zapata <alexzapata1984@gmail.com>
2025-06-22 11:42:32 +0200

    Make configuration changes and fix component import errors

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,6 +1,9 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
-/*import { ShoppingCartProvider } from '../../Context'*/
+import { PokedexProvider } from '../../Context'
 import Home from '../Home'
+import Navbar from '../../Components/Navbar'
+import PokemonCard from '../../Components/PokemonCard'
+
 /*
 import Navbar from '../../Components/Navbar'
 import CheckoutSideMenu from '../../Components/CheckoutSideMenu'*/
@@ -11,11 +14,11 @@ const AppRoutes = () => {
 
   const routes = useRoutes([
     { path: '/', element: <Home /> },
-    { path: '/clothes', element: <Home /> },
+    // { path: '/clothes', element: <Home /> },
     /*{ path: '/electronics', element: <Home /> },
     { path: '/furnitures', element: <Home /> },*/
     { path: '/pokemon-card', element: <PokemonCard /> },
-    { path: '/my-order', element: <MyOrder /> },
+    // { path: '/my-order', element: <MyOrder /> },
     /*{ path: '/my-orders', element: <MyOrders /> },
     { path: '/my-orders/last', element: <MyOrder /> },
     { path: '/my-orders/:id', element: <MyOrder /> },*/
@@ -29,13 +32,13 @@ const AppRoutes = () => {
 const App = () => {
 
   return (
-    <ShoppingCartProvider>
+    <PokedexProvider>
       <BrowserRouter>
         <AppRoutes />
         <Navbar />
-        <CheckoutSideMenu />
+        <PokemonCard />
       </BrowserRouter>
-    </ShoppingCartProvider>
+    </PokedexProvider>
   )
 }
 
