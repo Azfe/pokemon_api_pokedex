0d3b36a47e0dbee0248a5c55edf77d74e0edc6f1
Alex Zapata <alexzapata1984@gmail.com>
2025-06-22 13:30:04 +0200

    Add images: Import the images and modify the src path data

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,10 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { PokedexContext } from "../../Context";
+import pokemonLogo from '../../assets/pokemon_logo.svg';
 
 const Navbar = () => {
 
     return (        
         <nav className="bg-gray-800 p-4 text-white flex gap-4 ">
+            <div>
+                <img src={pokemonLogo} alt="Pokemon Logo" className="w-12 h-12" />
+            </div>
             <ul className="flex items-center gap-3">
                 <li>
                     <NavLink to="/" className="hover:text-gray-400">Home</NavLink>
--- src/Components/PokemonCard/index.jsx
+++ src/Components/PokemonCard/index.jsx
@@ -1,11 +1,11 @@
-
+import pokemonImg from '../../assets/004.png';
 
 const PokemonCard = () => {
 
     return (
         <div className="card">
         <h2>Pokemon Name</h2>
-        <img src="../../assets/004.png" alt="Pokemon" width="100" />
+        <img src={pokemonImg} alt="Pokemon" width="100" />
         <p>Type: Fire</p>
         <p>Abilities: Blaze, Solar Power</p>
         </div>
